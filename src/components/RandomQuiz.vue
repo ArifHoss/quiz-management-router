3854afacd2a1a200e64d5b4f6c01ed4e874b2490
arif <hossaininfo@ymail.com>
2021-11-03 15:46:46 +0100

    clickeDisable och onClickAnswer method added får response när svarar på frågorna

--- src/components/RandomQuiz.vue
+++ src/components/RandomQuiz.vue
@@ -15,7 +15,7 @@
       :questionCounter="thisQuestion.key"
       :key="answer"
       v-html="answer"
-      @click.prevent="onClickAnswer"
+      @click.prevent="clickDisable"
       ></button>
       <hr>
       <button @click="questionCounter++">Next</button>
@@ -51,12 +51,50 @@ export default {
       this.fetching = true
       let res = await fetch("http://127.0.0.1:3000/api/quizzes/10")
       let jsonRes = await res.json()
+      let questionCounter = 0
       let data = jsonRes.quiz.map((question) =>{
         question.answers = [ question.a1,question.a2, question.a3, question.a4]
+
+        question.correctAnswer = null
+        question.key = questionCounter
+        questionCounter++
         return question
       })
       this.questions = data
       this.fetching = false
+    },
+    clickDisable: function (event){
+      let questionCounter = event.target.getAttribute("questionCounter")
+      let polutedUserAnswer = event.target.innerHTML
+      let submittedAnswer = polutedUserAnswer.replace(/'/, "&#039;")
+      this.questions[questionCounter].submittedAnswer = submittedAnswer
+      event.target.classList.add("clicked")
+
+      let allButtons = document.querySelectorAll(`[questionCounter="${questionCounter}"]`)
+      for (let i = 0; i < allButtons.length; i++) {
+        if(allButtons[i] === event.target) continue
+        allButtons[i].setAttribute("disabled", "")
+      }
+      this.onClickAnswer(event, questionCounter)
+    },
+    onClickAnswer: function (event, questionCounter){
+      let question = this.questions[questionCounter]
+
+      if(question.submittedAnswer === question.corrct_answer){
+        event.target.classList.add("correctAnswer")
+        this.questions[questionCounter].correctAnswer = true
+      }else {
+        event.target.classList.add("incorrectAnswer")
+        this.questions[questionCounter].correctAnswer = false
+        let showCorrectAnswer = this.questions[questionCounter].correct_answer
+        let allAnswers = document.querySelectorAll(`[questionCounter="${questionCounter}"]`)
+        allAnswers.forEach(function (button){
+          if(button.innerHTML === showCorrectAnswer){
+            button.classList.add("displayCorrectAnswer")
+          }
+        })
+      }
+
     }
   },
   mounted() {
